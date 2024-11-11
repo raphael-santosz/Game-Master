df21e0b83ec79575740e1eb623261cb1e4cbdb7a
Raphael Augusto Santos <asantos.raphael@gmail.com>
2024-11-11 18:53:17 +0100

    Character implementation
    implementing the character core movimentation

---
+++ Computació_Gràfica_Game/Assets/Character.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class Character : MonoBehaviour
+{
+    public float Speed;
+    public float Jumpforce;
+
+    public bool isJumping;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move ()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+        }
+    }
+}
